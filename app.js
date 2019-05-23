f49ea519709a633d030844cc2b4bb587ea47f6ab
vincentbwalker <vincethewalker@gmail.com>
2019-05-23 08:51:20 +0000

    Added task manager in app.js and projected object in index.html

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
+        tasks: [
+          { id: 1, name: 'Item 1', description: 'Milk', completed: false},
+          { id: 2, name: 'Item 2', description: 'Bread', completed: true},
+        ],
         message: 'Hello World'
     }
 })
